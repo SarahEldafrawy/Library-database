cf52b5a68e2d8a96c6f379397a5663351e13ba9d
akrammousa <akrammousa2014@gmail.com>
2019-04-30 16:43:01 +0200

    fix conflicts.

--- src/Model/testConnectionModel.java
+++ src/Model/testConnectionModel.java
@@ -5,12 +5,12 @@ import java.sql.*;
 public class testConnectionModel {
     public static void main(String args[]){
         try{
-//            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/mysql","root","root");
+                    "jdbc:mysql://localhost:3306/book_store","akrammoussa","password");
             //here mysql is database name, root is username and password
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from emp");
+            ResultSet rs = stmt.executeQuery("select * from USER");
             while(rs.next())
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
             con.close();
