af3fc4b4fd394496d533c6ccb53e6ce43077730d
akrammousa <akrammousa2014@gmail.com>
2019-04-30 17:28:41 +0200

    conflict.

--- src/Model/testConnectionModel.java
+++ src/Model/testConnectionModel.java
@@ -1,20 +1,33 @@
 package Model;
 
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class testConnectionModel {
     public static void main(String args[]){
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/book_store","akrammoussa","password");
-            //here mysql is database name, root is username and password
+                    "jdbc:mysql://localhost:3306/book_store","root","password");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from USER");
+            ResultSet rs = stmt.executeQuery("select * from BOOK");
+            System.out.println(rs);
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            rs = stmt.executeQuery("select * from USER_PROFILE");
             while(rs.next())
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
             con.close();
-        }catch(Exception e){ System.out.println(e);}
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
