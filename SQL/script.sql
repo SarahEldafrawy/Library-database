935b9951411ac698660555a4243749c7d5682e2b
akrammousa <akrammousa2014@gmail.com>
2019-05-08 13:57:44 +0200

    Sql script.

--- SQL/script.sql
+++ SQL/script.sql
@@ -1,86 +1,273 @@
+create table if not exists PUBLISHER
+(
+  publisher_id int         not null,
+  name         varchar(50) not null,
+  phone        varchar(20) null,
+  address      varchar(50) null,
+  constraint `PRIMARY`
+  primary key (publisher_id)
+);
 
-    create table if not exists PUBLISHER
-    (
-      publisher_id int         not null,
-      name         varchar(50) not null,
-      phone        varchar(20) null,
-      address      varchar(50) null,
-      constraint `PRIMARY`
-      primary key (publisher_id)
-    );
+create table if not exists BOOK
+(
+  book_id       int                                                         not null,
+  title         varchar(50)                                                 not null,
+  pub_year      date                                                        null,
+  selling_price float                                                       not null,
+  category      enum ('Science', 'Art', 'Religion', 'History', 'Geography') null,
+  quantity      int(11) unsigned                                            not null,
+  publisher_id  int                                                         not null,
+  threshold     int(11) unsigned                                            not null,
+  constraint `PRIMARY`
+  primary key (book_id),
+  constraint BOOK_title_uindex
+  unique (title),
+  constraint BOOK_PUBLISHER_publisher_id_fk
+  foreign key (publisher_id) references PUBLISHER (publisher_id)
+    on update cascade
+);
 
-    create table if not exists BOOK
-    (
-      book_id       int                                                         not null,
-      title         varchar(50)                                                 not null,
-      pub_year      date                                                        null,
-      selling_price float                                                       not null,
-      category      enum ('Science', 'Art', 'Religion', 'History', 'Geography') null,
-      quantuty      int                                                         not null,
-      publisher_id  int                                                         not null,
-      constraint `PRIMARY`
-      primary key (book_id),
-      constraint BOOK_PUBLISHER_publisher_id_fk
-      foreign key (publisher_id) references PUBLISHER (publisher_id)
-        on update cascade
-    );
+create table if not exists Author
+(
+  Name      varchar(50) not null,
+  Author_id int         not null,
+  book_id   int         not null,
+  constraint `PRIMARY`
+  primary key (book_id, Author_id),
+  constraint Author_BOOK_Book_id_fk
+  foreign key (book_id) references BOOK (book_id)
+    on update cascade
+    on delete cascade
+);
 
-    create table if not exists Author
-    (
-      Name      varchar(50) not null,
-      Author_id int         not null,
-      book_id   int         not null,
-      constraint `PRIMARY`
-      primary key (book_id, Author_id),
-      constraint Author_BOOK_Book_id_fk
-      foreign key (book_id) references BOOK (book_id)
-        on update cascade
-        on delete cascade
-    );
+create trigger book_insertion
+  after INSERT
+  on BOOK
+  for each row
+  BEGIN
+    if (NEW.quantity < new.threshold)
+    then
+      insert into `ORDER` values (order_id, new.book_id, NEW.threshold - NEW.quantity, current_date);
+    end if;
+  END;
 
-    create table if not exists ORDERS
-    (
-      order_id int auto_increment,
-      constraint `PRIMARY`
-      primary key(order_id),
-      book_id  int  not null,
-      quantity int  not null,
-      date     date not null,
-      constraint ORDER_BOOK__fk
-      foreign key (book_id) references BOOK (book_id)
-        on update cascade
-        on delete cascade
-    );
+create trigger book_update
+  after UPDATE
+  on BOOK
+  for each row
+  BEGIN
+    declare sum_of_orders int default get_orders_quantity(NEW.book_id);
+    if (NEW.quantity + sum_of_orders < new.threshold)
+    then
+      insert into `ORDER`
+      values (order_id, new.book_id, new.threshold - NEW.quantity - sum_of_orders, current_date);
+    end if;
+  END;
 
-    create table if not exists USER_PROFILE
-    (
-      user_id int auto_increment,
-      constraint `PRIMARY`
-      primary key(user_id),
-      first_name       varchar(20) not null,
-      last_name        varchar(20) not null,
-      email_address    varchar(50) not null,
-      phone_number     varchar(20) null,
-      shipping_address varchar(50) not null,
-      password         varchar(50) not null,
-      promoted         tinyint(1)  not null,
-      constraint USER_email_address_uindex
-      unique (email_address)
-    );
+create table if not exists `ORDER`
+(
+  order_id int auto_increment
+  constraint `PRIMARY`
+  primary key,
+  book_id  int  not null,
+  quantity int  not null,
+  date     date not null,
+  constraint ORDER_BOOK__fk
+  foreign key (book_id) references BOOK (book_id)
+    on update cascade
+    on delete cascade
+);
 
-    create table if not exists CART
-    (
-      book_id  int not null,
-      user_id  int not null,
-      quantity int not null,
-      constraint `PRIMARY`
-      primary key (user_id, book_id),
-      constraint CART_BOOK_Book_id_fk
-      foreign key (book_id) references BOOK (book_id)
-        on update cascade
-        on delete cascade,
-      constraint CART_USER_user_id_fk
-      foreign key (user_id) references USER_PROFILE (user_id)
-        on update cascade
-        on delete cascade
-    );
+create trigger confirm_orders
+  after DELETE
+  on `ORDER`
+  for each row
+  BEGIN
+    update BOOK set BOOK.quantity = BOOK.quantity + OLD.quantity where BOOK.book_id = OLD.book_id;
+  END;
+
+create table if not exists USER
+(
+  user_id          int auto_increment
+  constraint `PRIMARY`
+  primary key,
+  first_name       varchar(20) not null,
+  last_name        varchar(20) not null,
+  email_address    varchar(50) not null,
+  phone_number     varchar(20) null,
+  shipping_address varchar(50) not null,
+  password         varchar(50) not null,
+  promoted         tinyint(1)  not null,
+  constraint USER_email_address_uindex
+  unique (email_address)
+);
+
+create table if not exists CART
+(
+  book_id  int                    not null,
+  user_id  int                    not null,
+  quantity int                    not null,
+  in_cart  tinyint(1) default '1' not null,
+  constraint `PRIMARY`
+  primary key (user_id, book_id),
+  constraint CART_BOOK_Book_id_fk
+  foreign key (book_id) references BOOK (book_id)
+    on update cascade
+    on delete cascade,
+  constraint CART_USER_user_id_fk
+  foreign key (user_id) references USER (user_id)
+    on update cascade
+    on delete cascade
+);
+
+create trigger check_out
+  after DELETE
+  on CART
+  for each row
+  BEGIN
+    if (old.in_cart = true)
+    then
+      call sell(OLD.book_id, OLD.user_id, old.quantity);
+    end if;
+
+  END;
+
+create trigger check_out_validation
+  before DELETE
+  on CART
+  for each row
+  BEGIN
+    if (book_quantity(OLD.book_id) < OLD.quantity)
+    then
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'there is not enough quantity in stock';
+    end if;
+
+  END;
+
+create table if not exists CREDIT_CARD
+(
+  user_id       int         not null,
+  credit_number varchar(20) not null,
+  constraint `PRIMARY`
+  primary key (user_id, credit_number),
+  constraint CREDIT_CARD_USER_user_id_fk
+  foreign key (user_id) references USER (user_id)
+    on update cascade
+    on delete cascade
+);
+
+create trigger credit_number_verification_insertion
+  before INSERT
+  on CREDIT_CARD
+  for each row
+  BEGIN
+    if (NOT (NEW.credit_number REGEXP
+             "[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$"))
+    then
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'credit card format is not right';
+    end if;
+  END;
+
+create trigger credit_number_verification_update
+  before INSERT
+  on CREDIT_CARD
+  for each row
+  BEGIN
+    if (NOT (NEW.credit_number REGEXP
+             "[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$"))
+    then
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'credit card format is not right';
+    end if;
+  END;
+
+create table if not exists SALES
+(
+  book_id      int  not null,
+  user_id      int  not null,
+  date_of_sale date not null,
+  quantity     int  not null,
+  constraint `PRIMARY`
+  primary key (user_id, book_id),
+  constraint SALES_BOOK_Book_id_fk
+  foreign key (book_id) references BOOK (book_id)
+    on update cascade
+    on delete cascade,
+  constraint SALES_USER_user_id_fk
+  foreign key (user_id) references USER (user_id)
+    on update cascade
+    on delete cascade
+);
+
+create function book_quantity(id int)
+  returns int
+  BEGIN
+    declare quantity int;
+    select BOOK.quantity into quantity from BOOK where BOOK.book_id = id;
+
+    return quantity;
+  END;
+
+create function get_orders_quantity(book_id int)
+  returns int
+  BEGIN
+    declare sum int;
+    select COALESCE(SUM(quantity), 0) into sum from `ORDER` where `ORDER`.book_id = book_id;
+    return sum;
+  END;
+
+create procedure purchase(IN user_id int)
+  BEGIN
+    DECLARE rollback bool default 0;
+    declare continue handler for SQLEXCEPTION set rollback = 1;
+    start transaction;
+    delete from CART where CART.user_id = user_id;
+    if rollback
+    then
+      ROLLBACK;
+    else
+      commit;
+    end if;
+  END;
+
+create procedure remove_from_cart(IN user_id int, IN book_id int)
+  BEGIN
+    DECLARE rollback bool default 0;
+    declare continue handler for SQLEXCEPTION set rollback = 1;
+    start transaction;
+    update CART
+    set CART.in_cart = false
+    where CART.user_id = user_id
+      AND CART.book_id = book_id;
+
+    delete
+    from CART
+    where CART.user_id = user_id
+      AND CART.book_id = book_id;
+
+    if rollback
+    then
+      ROLLBACK;
+    else
+      commit;
+    end if;
+  END;
+
+create procedure sell(IN book_id int, IN user_id int, IN quantity int)
+  BEGIN
+    DECLARE rollback bool default 0;
+    declare continue handler for SQLEXCEPTION set rollback = 1;
+    start transaction;
+    update BOOK set BOOK.quantity = BOOK.quantity - quantity where BOOK.book_id = book_id;
+    insert into SALES values (book_id, user_id, current_date, quantity);
+    if rollback
+    then
+      ROLLBACK;
+    else
+      commit;
+    end if;
+  END;
+
+
